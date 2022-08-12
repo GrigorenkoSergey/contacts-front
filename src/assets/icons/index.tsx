fc3ce8aa240512130cef37bd5ced0ffe2702800e
Sergey Grigorenko <sergey.grigorenko@metalamp.io>
2022-08-12 12:11:23 +0300

    removed loader. To fast even in slow connection

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -4,7 +4,6 @@ export { ReactComponent as DoorExit } from './door-exit.svg';
 export { ReactComponent as Email } from './email.svg';
 export { ReactComponent as EyeOff } from './eye-off.svg';
 export { ReactComponent as Eye } from './eye.svg';
-export { ReactComponent as Loader } from './loader.svg';
 export { ReactComponent as Options } from './options.svg';
 export { ReactComponent as Search } from './search.svg';
 export { ReactComponent as User } from './user.svg';
